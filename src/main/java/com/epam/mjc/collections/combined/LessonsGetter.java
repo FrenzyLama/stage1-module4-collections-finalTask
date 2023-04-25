ac08b0693c09e38a99ddee0ca05f376fdc7a18fc
Gleb Ilchyshyn <gleb.ilchyshyn@gmail.com>
2023-04-25 17:19:24 +0300

    Completed task

--- src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
+++ src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
@@ -1,10 +1,21 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class LessonsGetter {
     public Set<String> getLessons(Map<String, List<String>> timetable) {
+        Set<String> stringSet = new HashSet<>();
+
+        for (Map.Entry<String, List<String>> dayOfWeek: timetable.entrySet()) {
+            List<String> stringList = dayOfWeek.getValue();
+
+            for (String s : stringList) {
+                stringSet.add(s);
+            }
+        }
+        return stringSet;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,24 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> integerSetMap = new HashMap<>();
+
+        for (Map.Entry<String, Integer> entry: sourceMap.entrySet()) {
+            int keyLength = entry.getKey().length();
+
+            if (!integerSetMap.containsKey(keyLength)) {
+                Set<String> keysWithTheSameLength = new HashSet<>();
+
+                integerSetMap.put(keyLength, keysWithTheSameLength);
+            }
+
+            integerSetMap.get(keyLength).add(entry.getKey());
+
+        }
+
+        return integerSetMap;
     }
 }
